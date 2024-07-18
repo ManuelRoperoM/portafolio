1189bf7ded4a8449fe8e80781e8fccda2bc09471
ManuelRoperoM <mnlrpr@gmail.com>
2024-07-18 12:18:25 -0500

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Triqui from './Projects/triqui/Triqui'
+
+function App () {
+  return (
+    <>
+      <h1>Mi Portafolio</h1>
+      <Triqui />
+    </>
+  )
+}
+
+export default App
