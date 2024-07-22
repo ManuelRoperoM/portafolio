d94c8a533e7fb0bf10d1c33e3c2dabf17103676f
ManuelRoperoM <mnlrpr@gmail.com>
2024-07-22 17:49:18 -0500

    Add laboral experience

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,10 @@ import { mdiDatabaseCog } from '@mdi/js';
 import { mdiMessageText } from '@mdi/js';
 import { mdiGoogleAnalytics } from '@mdi/js';
 import { mdiBullhorn } from '@mdi/js';
+import { mdiAccountOutline } from '@mdi/js';
+import { mdiCogBox } from '@mdi/js';
+import { mdiAccountHardHatOutline } from '@mdi/js';
+import { mdiCodeBlockTags } from '@mdi/js';
 function App () {
   return (
     <>
@@ -31,7 +35,10 @@ function App () {
         <div className='presentacion'>
           <img src={photo} alt="Fotografia Manuel Ropero" />
           <div className='presentacion__descripcion'>
-            <h2>Manuel Ropero</h2>
+            <div className='title'>
+              <Icon path={mdiAccountOutline} size={2.2} /> 
+              <h2>Manuel Ropero</h2>
+            </div>
             <p>
               Soy Ingeniero en proceso de grado y desarrollador web <strong>Full Stack</strong> con una sólida experiencia en la creación y desarrollo de sitios y aplicaciones web utilizando diversas tecnologías. Me especializo en generar soluciones digitales que optimicen la experiencia del usuario, especialmente en el ámbito del <strong>ecommerce</strong>. <br />
             </p>
@@ -45,7 +52,10 @@ function App () {
         </div>
         <div className='skills'>
           <div className='skills__content'>
-            <h4>Habilidades</h4>
+            <div className='title'>
+              <Icon path={mdiCogBox} size={1.8} />
+              <h4>Habilidades</h4>
+            </div>
             <div className='skills-boxes'>
               <div className='skill-front'>
                 <Icon className='icon' path={mdiViewDashboardVariant} size={1.7} />
@@ -92,8 +102,99 @@ function App () {
             </div>
           </div>
         </div>
+        <div className='work-experience'>
+          <div className='title'>
+            <Icon path={mdiAccountHardHatOutline} size={1.8} />
+            <h3>Experiencia Laboral</h3>
+          </div>
+          <ul className='works'>
+            <li className='work'>
+              <div className='titles'>
+                <h4>Choho Colombia</h4>
+                <h5>Desarrollador full stack</h5>
+                <h6>Junio 2022 - mayo 2024 [2 años]</h6>
+              </div>
+              <p>
+                Desarrollé y mantuve funcionalidades del <strong>catálogo de ventas</strong> utilizando <strong>Nuxt.js</strong>, 
+                <strong>PHP</strong>, <strong>Nest.js</strong>, y <strong>PostgreSQL</strong>. Mejorando los procesos de <strong>ventas</strong>, <strong>facturación</strong>, y <strong>envíos</strong>, 
+                garantizando <strong>información</strong> actualizada y <strong>transacciones eficientes</strong>.
+              </p>
+            </li>
+            <li className='work'>
+              <div className='titles'>
+                <h4>Enel Colombia</h4>
+                <h5>Intership</h5>
+                <h6>febrero 2023 - agosto 2023 [7 meses]</h6>
+              </div>
+              <p>
+                Implementé un <strong>sistema automático</strong> en <strong>Python</strong> para consolidar información de macro 
+                medidores. Utilicé <strong>KNIME</strong>, <strong>JavaScript</strong>, <strong>HTML</strong>, <strong>CSS</strong>, <strong>Python</strong>, y <strong>SQLite</strong> como herramientas 
+                para el chat bot para mejorar la eficiencia y precisión en el mantenimiento de macro medidores.
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className='skills'>
+          <div className='skills__content'>
+            <div className='title'>           
+              <Icon path={mdiCodeBlockTags} size={1.8} />
+              <h4>Proyectos Personales</h4>
+            </div>
+            <div className='content'>
+              <div className='projects'>
+                <Triqui />
+                <ToDoProject />
+                <SearchMovies />
+                <Store/>
+              </div> 
+            </div>
+            {/* <div className='skills-boxes'>
+              <div className='skill-front'>
+                <Icon className='icon' path={mdiViewDashboardVariant} size={1.7} />
+                <h5>Front-End</h5>
+                <p>
+                  Puedo crear interfaces de usuario intuitivas y atractivas utilizando tecnologías como HTML, CSS, JavaScript y frameworks modernos como React. Me especializo en la optimización de la experiencia del usuario y el diseño responsivo.
+                </p>
+              </div>
+              <div className='skill-backend'>
+                <Icon className='icon' path={mdiServerNetwork} size={1.7} />
+                <h5>Back-End</h5>
+                <p>
+                  Desarrollo la lógica del servidor y API's eficientes con Node.js y Express. Garantizo un rendimiento óptimo y seguridad en el manejo de datos, además de integrar servicios externos y autenticación.
+                </p>
+              </div>
+              <div className='skill-database'>
+                <Icon className='icon'path={mdiDatabaseCog} size={1.7} />
+                <h5>Bases de datos</h5>
+                <p>
+                  Tengo experiencia en el diseño y gestión de bases de datos relacionales como MySQL, PostgreSQL y SQLite. Me enfoco en la optimización de consultas y la integridad de los datos.
+                </p>
+              </div>
+              <div className='skill-chatbots'>
+                <Icon className='icon' path={mdiMessageText} size={1.7} />
+                <h5>ChatBots</h5>
+                <p>
+                He desarrollado ChatBots inteligentes para plataformas como Telegram. <br />Estos bots están diseñados para recopilar información importante de manera rápida y eficaz, según las necesidades del usuario, mejorando así la interacción y automatizando tareas repetitivas.
+                </p>
+              </div>
+              <div className='skill-data-analytics'>
+                <Icon className='icon' path={mdiGoogleAnalytics} size={1.7} />
+                <h5>Analisis de datos</h5>
+                <p>
+                  Puedo mantener comunicación efectiva con el equipo de desarrollo, atendiendo cualquier inquietud que se presente para llevar el proyecto de manera correcta. Fomento la colaboración y el intercambio de ideas para alcanzar los objetivos del proyecto.
+                </p>
+              </div>
+              <div className='skill-comunication'>
+                <Icon className='icon' path={mdiBullhorn} size={1.7} />
+                <h5>Comunicación</h5>
+                <p>
+                  Puedo mantener comunicación efectiva con el equipo de desarrollo, atendiendo cualquier inquietud que se presente para llevar el proyecto de manera correcta. Fomento la colaboración y el intercambio de ideas para alcanzar los objetivos del proyecto.
+                </p>
+              </div>
+            </div> */}
+          </div>
+        </div>
       </main>
-
 
       {/* <div className='content'>
         <div className='projects'>
