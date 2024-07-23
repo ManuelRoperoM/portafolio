529fe8c9cb65fb17bfcd1ee4b1ba6aa957fe57d2
ManuelRoperoM <mnlrpr@gmail.com>
2024-07-22 19:31:46 -0500

    responsive desing

--- src/App.jsx
+++ src/App.jsx
@@ -33,11 +33,14 @@ function App () {
       </header>
       <main className='portafolio__main'>
         <div className='presentacion'>
-          <img src={photo} alt="Fotografia Manuel Ropero" />
+          <img className='photo-big' src={photo} alt="Fotografia Manuel Ropero" />
           <div className='presentacion__descripcion'>
             <div className='title'>
               <Icon path={mdiAccountOutline} size={2.2} /> 
               <h2>Manuel Ropero</h2>
+            </div>
+            <div className='photo-small'>
+              <img src={photo} alt="Fotografia Manuel Ropero" />
             </div>
             <p>
               Soy Ingeniero en proceso de grado y desarrollador web <strong>Full Stack</strong> con una sólida experiencia en la creación y desarrollo de sitios y aplicaciones web utilizando diversas tecnologías. Me especializo en generar soluciones digitales que optimicen la experiencia del usuario, especialmente en el ámbito del <strong>ecommerce</strong>. <br />
@@ -49,6 +52,38 @@ function App () {
               Mi versatilidad se refleja tanto en mis habilidades técnicas como en mi capacidad para trabajar en equipos ágiles utilizando metodologías <strong>SCRUM</strong>. Sobresalgo por mi comunicación asertiva y mi disposición a adquirir nuevos conocimientos de manera constante.
             </p>
           </div>
+        </div>
+        <div className='work-experience'>
+          <div className='title'>
+            <Icon path={mdiAccountHardHatOutline} size={1.8} />
+            <h3>Experiencia Laboral</h3>
+          </div>
+          <ul className='works'>
+            <li className='work'>
+              <div className='titles'>
+                <h4>Choho Colombia</h4>
+                <h5>Desarrollador full stack</h5>
+                <h6>Junio 2022 - mayo 2024 [2 años]</h6>
+              </div>
+              <p>
+                Desarrollé y mantuve funcionalidades del <strong>catálogo de ventas</strong> utilizando <strong>Nuxt.js</strong>, 
+                <strong>PHP</strong>, <strong>Nest.js</strong>, y <strong>PostgreSQL</strong>. Mejorando los procesos de <strong>ventas</strong>, <strong>facturación</strong>, y <strong>envíos</strong>, 
+                garantizando <strong>información</strong> actualizada y <strong>transacciones eficientes</strong>.
+              </p>
+            </li>
+            <li className='work'>
+              <div className='titles'>
+                <h4>Enel Colombia</h4>
+                <h5>Intership</h5>
+                <h6>febrero 2023 - agosto 2023 [7 meses]</h6>
+              </div>
+              <p>
+                Implementé un <strong>sistema automático</strong> en <strong>Python</strong> para consolidar información de macro 
+                medidores. Utilicé <strong>KNIME</strong>, <strong>JavaScript</strong>, <strong>HTML</strong>, <strong>CSS</strong>, <strong>Python</strong>, y <strong>SQLite</strong> como herramientas 
+                para el chat bot para mejorar la eficiencia y precisión en el mantenimiento de macro medidores.
+              </p>
+            </li>
+          </ul>
         </div>
         <div className='skills'>
           <div className='skills__content'>
@@ -101,38 +136,6 @@ function App () {
               </div>
             </div>
           </div>
-        </div>
-        <div className='work-experience'>
-          <div className='title'>
-            <Icon path={mdiAccountHardHatOutline} size={1.8} />
-            <h3>Experiencia Laboral</h3>
-          </div>
-          <ul className='works'>
-            <li className='work'>
-              <div className='titles'>
-                <h4>Choho Colombia</h4>
-                <h5>Desarrollador full stack</h5>
-                <h6>Junio 2022 - mayo 2024 [2 años]</h6>
-              </div>
-              <p>
-                Desarrollé y mantuve funcionalidades del <strong>catálogo de ventas</strong> utilizando <strong>Nuxt.js</strong>, 
-                <strong>PHP</strong>, <strong>Nest.js</strong>, y <strong>PostgreSQL</strong>. Mejorando los procesos de <strong>ventas</strong>, <strong>facturación</strong>, y <strong>envíos</strong>, 
-                garantizando <strong>información</strong> actualizada y <strong>transacciones eficientes</strong>.
-              </p>
-            </li>
-            <li className='work'>
-              <div className='titles'>
-                <h4>Enel Colombia</h4>
-                <h5>Intership</h5>
-                <h6>febrero 2023 - agosto 2023 [7 meses]</h6>
-              </div>
-              <p>
-                Implementé un <strong>sistema automático</strong> en <strong>Python</strong> para consolidar información de macro 
-                medidores. Utilicé <strong>KNIME</strong>, <strong>JavaScript</strong>, <strong>HTML</strong>, <strong>CSS</strong>, <strong>Python</strong>, y <strong>SQLite</strong> como herramientas 
-                para el chat bot para mejorar la eficiencia y precisión en el mantenimiento de macro medidores.
-              </p>
-            </li>
-          </ul>
         </div>
         <div className='skills'>
           <div className='skills__content'>
